eca4118d3411e260306a8302cf26db74f5472ada
Aaron Mina <doubledevwp@gmail.com>
2018-11-17 15:35:15 -0500

    Fix CRLF
    Added the rest of the Race and Subraces
    Still need to figure a better way

--- Race.cs
+++ Race.cs
@@ -20,6 +20,8 @@ using CharacterLib;
 //          - Half-Elf +2 Str, +2 Cha
 //          - Half-Orc + 1 Con
 //          - Tiefling +1 Int, +2 Cha
+// TODO: Make RaceModifiers return multiple types
+//          - Human, Dragonborn, Half-Elf, and Tiefling all have multiple stat boosts
 namespace RaceLib
 {
     public abstract class Race : Character
@@ -27,6 +29,7 @@ namespace RaceLib
         abstract public int RaceModifiers();
     }
 
+    // Dwarf +2 Con 
     public class Dwarf : Race
     {
         public override int RaceModifiers()
@@ -35,6 +38,7 @@ namespace RaceLib
         }
     }
 
+    // Mountain +2 Str
     public class MountainDwarf : Dwarf
     {
         public override int RaceModifiers()
@@ -43,6 +47,7 @@ namespace RaceLib
         }
     }
 
+    // Hill +1 Wis
     public class HillDwarf : Dwarf
     {
         public override int RaceModifiers()
@@ -51,6 +56,7 @@ namespace RaceLib
         }
     }
 
+    // Elf +2 Dex
     public class Elf : Race
     {
         public override int RaceModifiers()
@@ -59,6 +65,7 @@ namespace RaceLib
         }
     }
 
+    // Drow +1 Cha
     public class Drow : Elf
     {
         public override int RaceModifiers()
@@ -67,6 +74,7 @@ namespace RaceLib
         }
     }
 
+    // High +1 Int
     public class High : Elf
     {
         public override int RaceModifiers()
@@ -75,7 +83,112 @@ namespace RaceLib
         }
     }
 
+    // Wood +1 Wis
     public class Wood : Elf
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+
+    // Halfling +2 Dex
+    public class Halfling : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 2;
+        }
+    }
+
+    // Lightfoot +1 Cha
+    public class Lightfoot : Halfling
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+
+    // Stout +1 Con
+    public class Stout : Halfling
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+    
+    // Human +1 Str, +1 Dex, +1 Con, +1 Int, +1 Wis, +1 Cha
+    // TODO: How do I deal with this?
+    public class Human : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+            // return new[] {1, 1, 1, 1, 1, 1};
+        }
+    }
+
+    // Dragonborn +2 Str, +1 Cha
+    // TODO: How do I deal with this?
+    public class Dragonborn : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+    
+    // Gnome +2 Int
+    public class Gnome : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 2;
+        }
+    }
+
+    // Forest +2 Dex
+    public class Forest : Gnome
+    {
+        public override int RaceModifiers()
+        {
+            return 2;
+        }
+    }
+    
+    // Rock +1 Con
+    public class Rock : Gnome
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+    
+    // Half-Elf +2 Str, +2 Cha
+    // TODO: How do I deal with this?
+    public class Half_Elf : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 2;
+        }
+    }
+    
+    // Half-Orc + 1 Con
+    public class Half_Orc : Race
+    {
+        public override int RaceModifiers()
+        {
+            return 1;
+        }
+    }
+    
+    // Tiefling +1 Int, +2 Cha
+    // TODO: How do I deal with this?
+    public class Tiefling : Race
     {
         public override int RaceModifiers()
         {
